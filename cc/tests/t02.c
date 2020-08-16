fd3240a1fa04d415f56852c0b2f6abffb99b3307
Dibyendu Majumdar <mobile@majumdar.org.uk>
2020-08-16 14:46:54 +0100

    Add more tests and outputs

---
+++ cc/tests/t02.c
@@ -0,0 +1,9 @@
+main(argc, argv)
+int argc;
+char **argv;
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return 0;
+}
---
+++ cc/tests/t03.c
@@ -0,0 +1,11 @@
+adder(a, b, c)
+int a, b, c;
+{
+	return a+b+c;
+}
+
+double multipler(a, b, c)
+double a, b, c;
+{
+	return a*b*c;
+}
